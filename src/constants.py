89b5b9b3b1e8490fb3dd2ff77553a1f3bffe9929
Bryan Acosta <bxacosta@hotmail.com>
2025-03-19 05:25:55 -0500

    chore: add new global constants

--- src/constants.py
+++ src/constants.py
@@ -4,9 +4,11 @@ from typing import Final
 
 from src.enums import LaunchMode
 
+COMMAND_NAME: Final[str] = "warp"
 CONFIG_FILE_NAME: Final[str] = "config.json"
 LAUNCHER_SCRIPT_NAME: Final[str] = "launcher.vbs"
+PARENT_PROCESS_IDENTIFIER: Final[str] = "."
+INSTALL_PATH: Final[Path] = Path(os.environ.get("LOCALAPPDATA", Path.home())) / "Programs" / "WarpLauncher"
 
-DEFAULT_STARTING_PATH: Final[Path] = Path(".")
 DEFAULT_LAUNCH_MODE: Final[LaunchMode] = LaunchMode.WINDOW
-DEFAULT_INSTALL_PATH: Final[Path] = Path(os.environ.get("LOCALAPPDATA", Path.home())) / "Programs" / "WarpLauncher"
+DEFAULT_STARTING_PATH: Final[Path] = Path(PARENT_PROCESS_IDENTIFIER)
