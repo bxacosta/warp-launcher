6b67592c8f2050cd57ab499d4edcb4c0023bfec4
Bryan Acosta <bxacosta@hotmail.com>
2025-03-24 01:18:17 -0500

    chore: add constants for logger

--- src/constants.py
+++ src/constants.py
@@ -1,3 +1,4 @@
+import logging
 import os
 from pathlib import Path
 from typing import Final
@@ -7,8 +8,12 @@ from src.enums import LaunchMode
 COMMAND_NAME: Final[str] = "warp"
 CONFIG_FILE_NAME: Final[str] = "config.json"
 LAUNCHER_SCRIPT_NAME: Final[str] = "launcher.vbs"
-PARENT_PROCESS_IDENTIFIER: Final[str] = "."
 INSTALL_PATH: Final[Path] = Path(os.environ.get("LOCALAPPDATA", Path.home())) / "Programs" / "WarpLauncher"
+
+LOG_LEVEL = logging.INFO
+LOG_FORMAT = "%(message)" if LOG_LEVEL == logging.DEBUG else "%(message)s"
+
+PARENT_PROCESS_IDENTIFIER: Final[str] = "."
 
 DEFAULT_LAUNCH_MODE: Final[LaunchMode] = LaunchMode.WINDOW
 DEFAULT_STARTING_PATH: Final[Path] = Path(PARENT_PROCESS_IDENTIFIER)
