7d1aafdfe09a6c9476b5a7c8c176ec1fb0a93d2e
Bryan Acosta <bxacosta@hotmail.com>
2025-04-11 01:37:04 -0500

    refactor: update error handling in cli class

--- src/cli.py
+++ src/cli.py
@@ -1,6 +1,5 @@
 import argparse
 import logging
-import os
 import sys
 from typing import List, Optional
 
@@ -8,7 +7,6 @@ from src.constants import LOG_LEVEL, DEFAULT_COMMAND_NAME, DEFAULT_LAUNCH_MODE, 
 from src.enums import LaunchMode
 from src.launcher import Launcher
 from src.logger import setup_logger
-from src.utils import validate_path, validate_command_name
 
 logger = setup_logger(__name__)
 
@@ -46,26 +44,28 @@ def parse_cli_arguments(args: Optional[List[str]] = None) -> argparse.Namespace:
     )
 
     parser.add_argument(
-        '-i', '--install',
-        action='store_true',
-        help=f"Save the configuration, create the launch script and register the command."
-    )
-
-    parser.add_argument(
-        '-u', '--uninstall',
-        action='store_true',
-        help=f"Remove the launch script, configuration file and unregister the command."
-    )
-
-    parser.add_argument(
         '-v', '--verbose',
         action='store_true',
         help="Enable verbose logging."
     )
 
-    # Print help if no parameter is provided
+    action_group = parser.add_mutually_exclusive_group()
+    action_group.add_argument(
+        "-i", "--install",
+        action="store_true",
+        help=f"Save the configuration, create the launch script and register the command."
+    )
+    action_group.add_argument(
+        "-u", "--uninstall",
+        action="store_true",
+        help=f"Save the configuration, create the launch script and register the command."
+    )
+
+    # Use command-line args if not provided
     if args is None:
         args = sys.argv[1:]
+
+    # Print help if no parameter is provided
     if not args:
         parser.print_help()
         sys.exit(0)
@@ -80,51 +80,28 @@ def cli() -> int:
     # Set log level based on verbosity
     logging.getLogger().setLevel(logging.DEBUG if getattr(args, "verbose", False) else LOG_LEVEL)
 
-    launcher = Launcher()
-    config = launcher.config_handler.load_config()
+    try:
+        launcher = Launcher()
 
-    if getattr(args, "uninstall", False):
-        success, error_message = launcher.uninstall()
-        if not success:
-            logger.error(f"Failed to uninstall: {error_message}")
-            return 1
-        logger.info("Uninstallation completed successfully")
-        return 0
+        if getattr(args, "command", None):
+            launcher.command_name = args.command
 
-    if getattr(args, "command", None):
-        command_name, error_message = validate_command_name(args.command)
-        if not command_name:
-            logger.error(error_message)
-            return 1
-        config.command_name = command_name
-        logger.info(f"Command name set to '{config.command_name}'")
+        if getattr(args, "mode", None):
+            launcher.launch_mode = args.mode
 
-    if getattr(args, "mode", None):
-        launch_mode = LaunchMode.from_name(args.mode)
-        if not launch_mode:
-            logger.error(f"Invalid mode specified: '{args.mode}'")
-            return 1
-        config.launch_mode = launch_mode
-        logger.info(f"Launch mode set to '{config.launch_mode}'")
+        if getattr(args, "path", None):
+            launcher.launch_path = args.path
 
-    if getattr(args, "path", None):
-        launch_path, error_message = validate_path(args.path)
-        if not launch_path:
-            logger.error(error_message)
-            return 1
-        config.launch_path = launch_path
-        logger.info(f"Launch path set to '{config.launch_path}'")
+        if getattr(args, "launch", False):
+            launcher.launch_warp()
 
-    if getattr(args, "launch", False):
-        launcher.launch_warp(config)
-        resolved_path = os.getcwd() if config.is_launch_path_parent_process() else config.launch_path
-        logger.info(f"Warp launched in '{config.launch_mode}' mode at '{resolved_path}'")
+        if getattr(args, "install", False):
+            launcher.install()
 
-    if getattr(args, "install", False):
-        success, error_message = launcher.install(config)
-        if not success:
-            logger.error(f"Failed to save configuration: {error_message}")
-            return 1
-        logger.info(f"Installation completed successfully")
+        if getattr(args, "uninstall", False):
+            launcher.uninstall()
+    except Exception as e:
+        logger.error(e)
+        return 1
 
     return 0
